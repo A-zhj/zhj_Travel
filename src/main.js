9e65bb3365e410fb2f55ef620d94a76a9d2e1333
A-zhj <1337286028@qq.com>
2020-06-16 11:29:58 +0800

    第二次提交

--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import fastClick from 'fastclick'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
-import 'babel-polyfill'
 import store from './store'
-import 'styles/reset.css'
-import 'styles/border.css'
-import 'styles/iconfont.css'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+import './assets/styles/iconfont.css'
 import 'swiper/dist/css/swiper.css'
+import 'babel-polyfill'
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
